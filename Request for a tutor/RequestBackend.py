a10b2180970c6d3eebc8e648ba57b1e1126c1d34
tatsy56 <26dhakalt@tbs.edu.np>
2025-02-25 11:53:17 +0545

    backend for tutorrequest

--- Request for a tutor/RequestBackend.py
+++ Request for a tutor/RequestBackend.py
@@ -1,8 +1,17 @@
-from main import app,request
+from main import app,request,mysql, redirect, url_for, render_template
 
 
 
 @app.route('/RequestTutor',methods=['GET', 'POST'])
 def RequestTutor():
     if request.method == 'POST':
-        pass
+        subject = request.form['subject']
+        description = request.form['description']
+        cur = mysql.connection.cursor() 
+        cur.execute("INSERT INTO TutorReqests (subject, description) VALUES (%s, %s)", (subject, description))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('RequestTutor'))
+
+    return render_template('RequestTutor.html')
